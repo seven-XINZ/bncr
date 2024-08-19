0b4c1c77c319caf4e125b271a74b3939d0feeb1d
seven-xinz <114560460+seven-XINZ@users.noreply.github.com>
2024-08-19 13:13:07 +0800

    Update pixiv.js

--- plugins/xinz/pixiv.js
+++ plugins/xinz/pixiv.js
@@ -6,7 +6,7 @@
  * @description 完善版随机获取一组涩涩纸片人 首次使用请自行到web插件配置配置插件  机器自动科学可使用直连模式 
  需要存储图片则打开存储开关 图片自动存放到mod目录下
  * @rule ^zpr(18)?$
- * @admin true
+ * @admin false
  * @public false
  * @priority 50
  */
